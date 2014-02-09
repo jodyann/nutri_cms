4b9295b307215567d12466ee230e403de1a44de9
Jody Ann <annjodes@gmail.com>
2014-02-09 02:28:12 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NutriCms::Application.config.secret_key_base = 'ad94a5a86713f0a5f8b959307d10a914d508cf87b2f0328122e2795976b2ae82c25700631822dccb9c342db72c85946df92cd569d2a97b2134a2dbc3521b2bfd'
